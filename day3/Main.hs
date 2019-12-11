8abd1587319dd5c89bf721528abd5797ddf5644e
Marijn Bleeker <marijnbleeker@hotmail.com>
2019-12-11 21:23:21 +0100

    Add parsing for day 3

--- day3/Main.hs
+++ day3/Main.hs
@@ -1,4 +1,24 @@
+import Data.List.Split
+
+data Direction = Up | Right | Down | Left deriving Show
+
+type Instruction = (Direction, Int) 
+
 main :: IO ()
 main = do
-    print "Day 3"
+    content <- readFile "input"
+    let wires = splitOn "\n" content
+    let firstWire = splitOn "," (wires !! 0)
+    let secondWire = splitOn "," (wires !! 1)
+    putStrLn $ show $ parseInstruction $ head $ firstWire
+    putStrLn $ show firstWire
+
+parseInstruction :: String -> Instruction
+parseInstruction a = ((parseDirection $ head a) , read(tail a))
+
+parseDirection :: Char -> Direction
+parseDirection 'U' = Up
+parseDirection 'D' = Down
+parseDirection 'L' = Main.Left
+parseDirection 'R' = Main.Right
 
