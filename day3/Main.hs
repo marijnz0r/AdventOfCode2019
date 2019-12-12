c688efee68bc650578f6bd1c64ddbbab7b2bacb9
Marijn Bleeker <marijnbleeker@hotmail.com>
2019-12-12 08:04:26 +0100

    Add addPoint function

--- day3/Main.hs
+++ day3/Main.hs
@@ -3,6 +3,7 @@ import Data.List.Split
 data Direction = Up | Right | Down | Left deriving Show
 
 type Instruction = (Direction, Int) 
+type Point = (Int, Int)
 
 main :: IO ()
 main = do
@@ -10,8 +11,11 @@ main = do
     let wires = splitOn "\n" content
     let firstWire = splitOn "," (wires !! 0)
     let secondWire = splitOn "," (wires !! 1)
-    putStrLn $ show $ parseInstruction $ head $ firstWire
-    putStrLn $ show firstWire
+    let firstWireInstructions = map parseInstruction firstWire
+    let secondWireInstructions = map parseInstruction secondWire
+    let firstWireMap = Map.fromList [(0,0)]
+    putStrLn $ show firstWireInstructions 
+    -- putStrLn $ show $ map parseInstruction firstWire
 
 parseInstruction :: String -> Instruction
 parseInstruction a = ((parseDirection $ head a) , read(tail a))
@@ -22,3 +26,5 @@ parseDirection 'D' = Down
 parseDirection 'L' = Main.Left
 parseDirection 'R' = Main.Right
 
+addPoint :: [Point] -> Point -> [Point]
+addPoint points point = points : point
